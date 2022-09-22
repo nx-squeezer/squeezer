08f3f34efcb48ba8cc2e69f8cf281fb4b7be2390
Samuel Fernández <samuelfernandez@users.noreply.github.com>
2022-09-22 15:06:50 +0200

    test(workspace): :white_check_mark: fix normalize join test

--- packages/workspace/src/generators/core/join-normalize.spec.ts
+++ packages/workspace/src/generators/core/join-normalize.spec.ts
@@ -2,7 +2,7 @@ import { joinNormalize } from './join-normalize';
 
 describe('@nx-squeezer/workspace joinNormalize', () => {
   it('convert backwards-slash paths to forward slash paths', () => {
-    expect(joinNormalize('c:/aaaa', '\\bbbb')).toBe('c:/aaaa/bbbb');
-    expect(joinNormalize('c:\\aaaa', '\\bbbb')).toBe('c:/aaaa/bbbb');
+    expect(joinNormalize('c:/aaaa', 'bbbb')).toBe('c:/aaaa/bbbb');
+    expect(joinNormalize('c:\\aaaa', 'bbbb')).toBe('c:/aaaa/bbbb');
   });
 });
