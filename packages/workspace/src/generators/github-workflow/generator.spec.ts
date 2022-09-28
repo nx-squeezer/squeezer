75357a466c4bb8dfd60c19b3e4fa48cb210f9a20
Samuel Fernández <samuelfernandez@users.noreply.github.com>
2022-09-28 15:46:49 +0200

    test(workspace): :white_check_mark: add full coverage for missing generators (#29)

--- packages/workspace/src/generators/github-workflow/generator.spec.ts
+++ packages/workspace/src/generators/github-workflow/generator.spec.ts
@@ -1,7 +1,8 @@
 import { NxJsonConfiguration, readJson, Tree } from '@nrwl/devkit';
 import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
+import { parse } from 'yaml';
 
-import { ciFile, readPackageJson } from '../core';
+import { ciFile, existsGitHubCiWorkflow, readPackageJson } from '../core';
 import { readmeFile } from '../core/add-badge-to-readme';
 import { getGitRepo } from '../core/get-git-repo';
 import { nxConfigFile } from '../core/nx';
@@ -15,15 +16,38 @@ describe('@nx-squeezer/workspace github workflow generator', () => {
   beforeEach(() => {
     tree = createTreeWithEmptyWorkspace();
     jest.spyOn(console, 'log').mockImplementation(() => null);
+    jest.spyOn(console, 'error').mockImplementation(() => null);
     (getGitRepo as jest.Mock).mockReturnValue('https://github.com/test/test');
   });
 
   it('should run successfully', async () => {
     await generator(tree, { branch: 'main', useNxCloud: true, force: true });
 
-    const ciWorkflow = tree.read(ciFile)?.toString();
+    expect(existsGitHubCiWorkflow(tree)).toBeTruthy();
+  });
 
-    expect(ciWorkflow).toBeDefined();
+  it('should skip execution if a GitHub CI workflow already exists', async () => {
+    tree.write(ciFile, '');
+
+    await generator(tree, { branch: 'main', useNxCloud: true, force: false });
+
+    expect(console.log).toHaveBeenCalledWith(`GitHub workflow already existing at path: ${ciFile}`);
+  });
+
+  it('should not skip execution if a GitHub CI workflow already exists but passing force option', async () => {
+    tree.write(ciFile, '');
+
+    await generator(tree, { branch: 'main', useNxCloud: true, force: true });
+
+    expect(tree.read(ciFile)?.toString()).toBeTruthy();
+  });
+
+  it('should generate the GitHub CI workflow', async () => {
+    await generator(tree, { branch: 'main', useNxCloud: true, force: true });
+
+    const ci = parse(tree.read(ciFile)?.toString() ?? '');
+
+    expect(ci).toStrictEqual(ciFileContent());
   });
 
   it('should declare the nx script in package.json', async () => {
@@ -32,6 +56,7 @@ describe('@nx-squeezer/workspace github workflow generator', () => {
     const packageJson = readPackageJson(tree);
 
     expect(packageJson.scripts?.nx).toBe('nx');
+    expect(packageJson.scripts?.['lint:workspace']).toBe('nx workspace-lint');
   });
 
   it('should declare the implicit dependency in nx.json', async () => {
@@ -50,4 +75,247 @@ describe('@nx-squeezer/workspace github workflow generator', () => {
 
     expect(readme).toContain('[![CI]');
   });
+
+  it('should not add a badge in readme if repo can not be resolved', async () => {
+    (getGitRepo as jest.Mock).mockReturnValue(null);
+    tree.write(readmeFile, '# Readme\n');
+    await generator(tree, { branch: 'main', useNxCloud: true, force: true });
+
+    const readme = tree.read(readmeFile)?.toString() ?? '';
+
+    expect(readme).not.toContain('[![CI]');
+    expect(console.error).toHaveBeenCalledWith(`Could not add badge to README, remote repo could not be detected.`);
+  });
+});
+
+/* eslint-disable sonarjs/no-duplicate-string */
+const ciFileContent = () => ({
+  name: 'CI',
+  on: {
+    push: {
+      branches: ['main'],
+    },
+    pull_request: {
+      branches: ['main'],
+    },
+  },
+  jobs: {
+    npm: {
+      name: 'NPM',
+      'runs-on': 'ubuntu-latest',
+      env: {
+        NX_RUN_GROUP: '${{ github.run_id }}',
+        NX_CLOUD_AUTH_TOKEN: '${{ secrets.NX_CLOUD_AUTH_TOKEN }}',
+      },
+      steps: [
+        {
+          name: 'Checkout repo',
+          uses: 'actions/checkout@v3.0.2',
+        },
+        {
+          name: 'Setup Node.js',
+          uses: 'actions/setup-node@v3.4.1',
+          with: {
+            'node-version': '16.x',
+          },
+        },
+        {
+          name: 'NPM',
+          uses: 'ng-easy/npm-setup@v2.0.10',
+        },
+      ],
+    },
+    build: {
+      name: 'Build',
+      needs: ['npm'],
+      'runs-on': 'ubuntu-latest',
+      env: {
+        NX_RUN_GROUP: '${{ github.run_id }}',
+        NX_CLOUD_AUTH_TOKEN: '${{ secrets.NX_CLOUD_AUTH_TOKEN }}',
+      },
+      steps: [
+        {
+          name: 'Checkout repo',
+          uses: 'actions/checkout@v3.0.2',
+          with: {
+            'fetch-depth': 0,
+          },
+        },
+        {
+          name: 'Setup Node.js',
+          uses: 'actions/setup-node@v3.4.1',
+          with: {
+            'node-version': '16.x',
+          },
+        },
+        {
+          name: 'NPM',
+          uses: 'ng-easy/npm-setup@v2.0.10',
+          with: {
+            'nx-key': 'build',
+          },
+        },
+        {
+          name: 'Nx Set SHAs',
+          uses: 'nrwl/nx-set-shas@v2.2.7',
+        },
+        {
+          name: 'Build',
+          uses: 'mansagroup/nrwl-nx-action@v2.1.0',
+          with: {
+            targets: 'build',
+            affected: 'true',
+            parallel: 'true',
+            maxParallel: 3,
+            nxCloud: 'true',
+          },
+        },
+      ],
+    },
+    lint: {
+      name: 'Lint',
+      needs: ['npm'],
+      'runs-on': 'ubuntu-latest',
+      env: {
+        NX_RUN_GROUP: '${{ github.run_id }}',
+        NX_CLOUD_AUTH_TOKEN: '${{ secrets.NX_CLOUD_AUTH_TOKEN }}',
+      },
+      steps: [
+        {
+          name: 'Checkout repo',
+          uses: 'actions/checkout@v3.0.2',
+          with: {
+            'fetch-depth': 0,
+          },
+        },
+        {
+          name: 'Setup Node.js',
+          uses: 'actions/setup-node@v3.4.1',
+          with: {
+            'node-version': '16.x',
+          },
+        },
+        {
+          name: 'NPM',
+          uses: 'ng-easy/npm-setup@v2.0.10',
+          with: {
+            'nx-key': 'lint',
+          },
+        },
+        {
+          name: 'Nx Set SHAs',
+          uses: 'nrwl/nx-set-shas@v2.2.7',
+        },
+        {
+          name: 'Lint',
+          uses: 'mansagroup/nrwl-nx-action@v2.1.0',
+          with: {
+            targets: 'lint',
+            affected: 'true',
+            parallel: 'true',
+            maxParallel: 3,
+            nxCloud: 'true',
+          },
+        },
+        {
+          name: 'lint workspace',
+          run: 'npm run lint:workspace',
+        },
+      ],
+    },
+    test: {
+      name: 'Test',
+      needs: ['npm'],
+      'runs-on': 'ubuntu-latest',
+      env: {
+        NX_RUN_GROUP: '${{ github.run_id }}',
+        NX_CLOUD_AUTH_TOKEN: '${{ secrets.NX_CLOUD_AUTH_TOKEN }}',
+      },
+      steps: [
+        {
+          name: 'Checkout repo',
+          uses: 'actions/checkout@v3.0.2',
+          with: {
+            'fetch-depth': 0,
+          },
+        },
+        {
+          name: 'Setup Node.js',
+          uses: 'actions/setup-node@v3.4.1',
+          with: {
+            'node-version': '16.x',
+          },
+        },
+        {
+          name: 'NPM',
+          uses: 'ng-easy/npm-setup@v2.0.10',
+          with: {
+            'nx-key': 'test',
+          },
+        },
+        {
+          name: 'Nx Set SHAs',
+          uses: 'nrwl/nx-set-shas@v2.2.7',
+        },
+        {
+          name: 'Test',
+          uses: 'mansagroup/nrwl-nx-action@v2.1.0',
+          with: {
+            targets: 'test',
+            affected: 'true',
+            parallel: 'true',
+            maxParallel: 3,
+            nxCloud: 'true',
+          },
+        },
+      ],
+    },
+    e2e: {
+      name: 'e2e',
+      needs: ['npm'],
+      'runs-on': 'ubuntu-latest',
+      env: {
+        NX_RUN_GROUP: '${{ github.run_id }}',
+        NX_CLOUD_AUTH_TOKEN: '${{ secrets.NX_CLOUD_AUTH_TOKEN }}',
+      },
+      steps: [
+        {
+          name: 'Checkout repo',
+          uses: 'actions/checkout@v3.0.2',
+          with: {
+            'fetch-depth': 0,
+          },
+        },
+        {
+          name: 'Setup Node.js',
+          uses: 'actions/setup-node@v3.4.1',
+          with: {
+            'node-version': '16.x',
+          },
+        },
+        {
+          name: 'NPM',
+          uses: 'ng-easy/npm-setup@v2.0.10',
+          with: {
+            'nx-key': 'e2e',
+          },
+        },
+        {
+          name: 'Nx Set SHAs',
+          uses: 'nrwl/nx-set-shas@v2.2.7',
+        },
+        {
+          name: 'e2e',
+          uses: 'mansagroup/nrwl-nx-action@v2.1.0',
+          with: {
+            targets: 'e2e',
+            affected: 'true',
+            parallel: 'true',
+            maxParallel: 3,
+            nxCloud: 'true',
+          },
+        },
+      ],
+    },
+  },
 });
