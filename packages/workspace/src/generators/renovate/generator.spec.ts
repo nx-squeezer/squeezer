d6d5e1f9f2c56e3e7b62dec6ea06875d9d04396c
Samuel Fernández <samuelfernandez@users.noreply.github.com>
2023-01-04 09:25:03 +0100

    fix(workspace): :bug: use npm script for migration script

--- packages/workspace/src/generators/renovate/generator.spec.ts
+++ packages/workspace/src/generators/renovate/generator.spec.ts
@@ -233,7 +233,7 @@ const renovateConfigJsContent = () =>
     '^npx --no-install ng update (@[a-z0-9-~][a-z0-9-._~]*\\/)?[a-z0-9-~][a-z0-9-._~]* --from=\\d+\\.\\d+\\.\\d+ --to=\\d+\\.\\d+\\.\\d+ --migrate-only --allow-dirty --force$',
     '^npx --no-install ng lint --fix$',
     '^npx --no-install nx migrate (@[a-z0-9-~][a-z0-9-._~]*\\/)?[a-z0-9-~][a-z0-9-._~]* --from=(@[a-z0-9-~][a-z0-9-._~]*\\/)?[a-z0-9-~][a-z0-9-._~]*@\\d+\\.\\d+\\.\\d+ --to=(@[a-z0-9-~][a-z0-9-._~]*\\/)?[a-z0-9-~][a-z0-9-._~]*@\\d+\\.\\d+\\.\\d+$',
-    '^npx --no-install path-exists-cli migrations\\.json && npx --no-install nx migrate --run-migrations=migrations\\.json$',
+    '^npm run migrations$',
     '^rm -f migrations.json || true$',
     '^npx --no-install nx workspace-lint$',
     '^npx --no-install nx run-many --target=lint --all --parallel --fix --skip-nx-cache$',
