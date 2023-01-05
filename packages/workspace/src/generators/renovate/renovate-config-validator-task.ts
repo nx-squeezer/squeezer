1c1aeb8ab162e7835a8218af6d0cd53d9dce0d51
Samuel Fernández <samuelfernandez@users.noreply.github.com>
2023-01-05 11:30:24 +0100

    fix(workspace): :bug: renovate config validator task will succeed even if not installed
    It now uses a full command that does not rely on having the package installed locally. Taken from https://github.com/rinchsan/renovate-config-validator/blob/main/src/main.ts
    
    Closes #259

--- packages/workspace/src/generators/renovate/renovate-config-validator-task.ts
+++ packages/workspace/src/generators/renovate/renovate-config-validator-task.ts
@@ -4,13 +4,16 @@ import { exec } from '../lib';
 
 export function renovateConfigValidatorTask(tree: Tree): boolean {
   console.log(`Validating Renovate configuration...`);
-  const { output, error } = exec('npx', ['renovate-config-validator'], {
+  const { output, error } = exec('npx', ['--package', 'renovate', '-c', 'renovate-config-validator'], {
     cwd: tree.root,
   });
 
   if (error != null) {
+    console.error(error);
     return false;
   }
+
+  console.log(output);
 
   return output.includes(`Config validated successfully`);
 }
