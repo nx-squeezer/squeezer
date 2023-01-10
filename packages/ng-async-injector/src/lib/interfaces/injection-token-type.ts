8eb68513ac76555e17dac7e95f6ad3711f42ae36
Samuel Fernández <samuelfernandez@users.noreply.github.com>
2023-01-10 13:02:14 +0100

    feat(ng-async-injector): :sparkles: directive support template resolving (#300) (#304)
    Closes #300

--- packages/ng-async-injector/src/lib/interfaces/injection-token-type.ts
+++ packages/ng-async-injector/src/lib/interfaces/injection-token-type.ts
@@ -2,6 +2,10 @@ import { InjectionToken } from '@angular/core';
 
 export type InjectionTokenType<T extends InjectionToken<any>> = T extends InjectionToken<infer R> ? R : never;
 
-export type InjectionTokenTypes<T extends InjectionToken<any>[]> = {
+export type InjectionTokenTypeCollection<T extends InjectionToken<any>[]> = {
+  [K in keyof T]: InjectionTokenType<T[K]>;
+};
+
+export type InjectionTokenTypeMap<T extends { [key: string]: InjectionToken<any> }> = {
   [K in keyof T]: InjectionTokenType<T[K]>;
 };
