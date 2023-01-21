d7ed7fbea16ce332ce6c0adb9cce7ccdeea292af
Samuel Fernández <samuelfernandez@users.noreply.github.com>
2023-01-21 18:00:09 +0100

    chore(deps): ⬆️ update nrwl workspace (#356)

--- packages/ng-async-injector-e2e/src/app/app.component.ts
+++ packages/ng-async-injector-e2e/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'nx-squeezer-root',
   imports: [RouterOutlet],
-  template: `<router-outlet/>`,
+  template: `<router-outlet />`,
   standalone: true,
 })
 export class AppComponent {}
