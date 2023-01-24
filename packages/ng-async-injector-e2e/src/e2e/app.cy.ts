3709d00f72a68be496359892167cf07d547c654d
Samuel Fernández <samuelfernandez@users.noreply.github.com>
2023-01-24 11:53:23 +0100

    test(ng-async-injector-e2e): :white_check_mark: add e2e tests for ng-async-injector using cypress
    Closes #345

---
+++ packages/ng-async-injector-e2e/src/e2e/app.cy.ts
@@ -0,0 +1,11 @@
+describe('ng-async-injector-app', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('should provide all async providers', () => {
+    cy.contains('Provided with useAsyncValue');
+    cy.contains('Provided with useAsyncFactory');
+    cy.contains('Provided with useAsyncClass');
+    cy.contains('Resolved through directive with key');
+    cy.contains('Resolved through directive with implicit context');
+  });
+});
