69533403955064b6621ad8731bd6052cda0dc2b1
Samuel Fernández <samuelfernandez@users.noreply.github.com>
2023-04-10 15:43:23 +0200

    chore: :recycle: simplify project configurations and use target defaults (#578) (#589)

--- packages/workspace/jest.config.ts
+++ packages/workspace/jest.config.ts
@@ -5,14 +5,4 @@ export default {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/packages/workspace',
-  coverageReporters: ['lcov'],
-  coverageThreshold: {
-    global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
-    },
-  },
 };
