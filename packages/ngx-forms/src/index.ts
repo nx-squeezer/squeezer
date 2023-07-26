104c70baa1ee64eebc5d6bff5aaa5e5b8a45b6d6
Samuel Fernández <samuelfernandez@users.noreply.github.com>
2023-07-26 08:56:42 +0200

    feat(ngx-forms): :tada: scaffold ngx-forms library
    Closes #374

---
+++ packages/ngx-forms/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/ngx-forms/ngx-forms.component';
---
+++ packages/ngx-forms/src/lib/ngx-forms/ngx-forms.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-forms',
+  standalone: true,
+  template: ``,
+})
+export class NgxFormsComponent {}
