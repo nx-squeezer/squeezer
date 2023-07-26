753aa6afbb070794c06f283cccf6f68ae2e472f5
Samuel Fernández <samuelfernandez@users.noreply.github.com>
2023-07-26 12:45:15 +0200

    feat(ngx-forms-app): :sparkles: create e2e app and fix change detection (#841)

--- packages/ngx-forms/src/lib/validators/lazy-validator.ts
+++ packages/ngx-forms/src/lib/validators/lazy-validator.ts
@@ -1,14 +1,15 @@
-import { inject } from '@angular/core';
+import { inject, ChangeDetectorRef } from '@angular/core';
 import { AsyncValidator, AbstractControl, ValidationErrors } from '@angular/forms';
 
-import { lazyValidator } from '../functions/lazy-validator';
 import { LOAD_VALIDATOR } from '../tokens/load-validator.token';
 
 export class LazyValidator implements AsyncValidator {
   private readonly loadValidatorFn = inject(LOAD_VALIDATOR);
+  private readonly cdr = inject(ChangeDetectorRef);
 
   async validate(control: AbstractControl): Promise<ValidationErrors | null> {
-    const validator = lazyValidator(this.loadValidatorFn);
+    const validator = await this.loadValidatorFn();
+    this.cdr.markForCheck();
     return validator(control);
   }
 }
