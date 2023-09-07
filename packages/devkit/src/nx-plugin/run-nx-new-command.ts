5aca28c0391fe2d4d46a4b29d66abcf11aa7068b
Samuel Fernández <samuelfernandez@users.noreply.github.com>
2023-09-07 21:15:40 +0200

    fix(devkit): fix run nx command

--- packages/devkit/src/nx-plugin/run-nx-new-command.ts
+++ packages/devkit/src/nx-plugin/run-nx-new-command.ts
@@ -19,7 +19,7 @@ export function runNxNewCommand(): void {
       '--skip-install',
       '--collection=@nx/workspace',
       '--npmScope=proj',
-      '--preset=empty',
+      '--preset=apps',
     ],
     { cwd: localTmpDir }
   );
