af3639b3f88e29dd3bdc1ae1fb29b9e53ceafc15
Samuel Fernández <samuelfernandez@users.noreply.github.com>
2023-10-06 13:58:26 +0200

    feat(ngx-ssr-app): :sparkles: scaffold SSR app (#979)

---
+++ e2e/ngx-ssr-app/jest.config.ts
@@ -0,0 +1,33 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  displayName: 'ngx-ssr-app',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  coverageDirectory: '../../coverage/e2e/ngx-ssr-app',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+};
+
+export default config;
