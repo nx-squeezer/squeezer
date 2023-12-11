4e78f2c7dc014099b50ed62902a08859b696895a
Samuel Fernández <samuelfernandez@users.noreply.github.com>
2023-12-11 14:22:50 +0000

    fix: upgrade nx to v17.2.0

---
+++ nx-migrate-latest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+rm package-lock.json
+rm -rf node_modules
+npm install
+npx nx migrate latest
+npx nx migrate @angular/core@latest
+npx nx migrate @angular/cli@latest
+npx nx migrate @jscutlery/semver@latest
+npx nx migrate ngx-deploy-npm@latest
+npx nx migrate @enio.ai/typedoc@latest
+npm install
+npx nx migrate --run-migrations --if-exists
+rm -f migrations.json
+npx nx run-many --target=lint --all --parallel=3 --fix --skip-nx-cache
