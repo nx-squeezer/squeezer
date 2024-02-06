bf11ce27b1e8a341d7108267955e131fc19b74df
samuelfernandez <samuelfernandez@users.noreply.github.com>
2024-02-06 17:45:08 +0000

    Deploying to gh-pages from @ nx-squeezer/squeezer@ba576f1643f5bec95648e72c24c1b838ba0d186b 🚀

--- packages/utils/assets/navigation.js
+++
@@ -1,1 +0,0 @@
-window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA3XMywpAQACF4Xc568ktl8yap5DFxIgwJo4aybvbKcX6//uqE9SOkCh0p/aJpbPLSghYxR4SPKze/Ff0es4TBMbBtJBRkGdhEl3ikbTjqhr+gN1uGg6L2fyv722n8VXfdS8RxqIAAAA="
