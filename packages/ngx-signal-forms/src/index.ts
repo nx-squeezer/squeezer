149ad1301569211540bc07fffb2529e68e8e154b
Samuel Fernández <samuelfernandez@users.noreply.github.com>
2024-02-08 15:58:41 +0100

    feat(ngx-signal-forms): :tada: scaffold signal forms library

---
+++ packages/ngx-signal-forms/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/ngx-signal-forms/ngx-signal-forms.component';
---
+++ packages/ngx-signal-forms/src/lib/ngx-signal-forms/ngx-signal-forms.component.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './ngx-signal-forms.component.html',
+  styleUrl: './ngx-signal-forms.component.css',
+})
+export class NgxSignalFormsComponent {}
