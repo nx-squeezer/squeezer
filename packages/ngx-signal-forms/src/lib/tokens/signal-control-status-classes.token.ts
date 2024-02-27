cf249dbea5bbc72131efa75fa232ce06b4f5b716
Samuel Fernández <samuelfernandez@users.noreply.github.com>
2024-02-27 16:15:49 +0100

    feat(ngx-signal-forms): :sparkles: provide configuration for css classes added by status (#1284)

---
+++ packages/ngx-signal-forms/src/lib/tokens/signal-control-status-classes.token.ts
@@ -0,0 +1,18 @@
+import { InjectionToken } from '@angular/core';
+
+import { SignalControlStatusClasses } from '../models/signal-control-status-classes';
+
+/**
+ * This token defines the CSS classes that are added to controls depending on their status.
+ */
+export const SIGNAL_CONTROL_STATUS_CLASSES = new InjectionToken('ngx-signal-forms.signal-control-status-classes', {
+  providedIn: 'root',
+  factory(): SignalControlStatusClasses {
+    return {
+      valid: 'ngx-valid',
+      invalid: 'ngx-invalid',
+      pristine: 'ngx-pristine',
+      dirty: 'ngx-dirty',
+    };
+  },
+});
