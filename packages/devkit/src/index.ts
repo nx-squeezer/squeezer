30fd89439a3c13222cbec31c953f14f446fe8227
Samuel Fernández <samuelfernandez@users.noreply.github.com>
2024-07-12 12:51:38 +0200

    fix(devkit): :boom: remove utils no longer needed to run e2e tests for nx plugins
    BREAKING CHANGE Remove nx plugin testing workarounds

--- packages/devkit/src/index.ts
+++ packages/devkit/src/index.ts
@@ -4,7 +4,6 @@ export * from './github-workflow';
 export * from './husky';
 export * from './npm';
 export * from './nx';
-export * from './nx-plugin';
 export * from './package-json';
 export * from './path';
 export * from './readme';
