acad17d559f3fa1544ea8b64940bbc2fa8341fe1
Samuel Fernández <samuelfernandez@users.noreply.github.com>
2024-07-12 12:55:03 +0200

    test(devkit): update e2e test to use verdaccio (#1578)

--- e2e/devkit-e2e/tests/devkit.spec.ts
+++ e2e/devkit-e2e/tests/devkit.spec.ts
@@ -1,28 +1,49 @@
-import { runNxCommandAsync, uniq } from '@nx/plugin/testing';
+import { execSync } from 'child_process';
+import { mkdirSync, rmSync } from 'fs';
+import { basename, dirname } from 'path';
 
-import { ensureNxProject } from '@nx-squeezer/devkit';
+import { tmpProjPath } from '@nx/plugin/testing';
 
-describe('devkit e2e', () => {
-  // Setting up individual workspaces per
-  // test can cause e2e runs to take a long time.
-  // For this reason, we recommend each suite only
-  // consumes 1 workspace. The tests should each operate
-  // on a unique project in the workspace, such that they
-  // are not dependant on one another.
+describe('@nx-squeezer/devkit e2e', () => {
+  let projectDirectory: string;
+
   beforeAll(() => {
-    ensureNxProject('devkit');
+    projectDirectory = createTestProject();
+
+    // The plugin has been built and published to a local registry in the jest globalSetup
+    // Install the plugin built with the latest source code into the test repo
+    execSync(`npm install @nx-squeezer/devkit@e2e`, { cwd: projectDirectory, stdio: 'inherit', env: process.env });
   });
 
   afterAll(() => {
-    // `nx reset` kills the daemon, and performs
-    // some work which can help clean up e2e leftovers
-    runNxCommandAsync('reset');
+    // Cleanup the test project
+    rmSync(projectDirectory, { recursive: true, force: true });
   });
 
-  it('should create devkit', async () => {
-    const project = uniq('devkit');
-    // await runNxCommandAsync(`generate @nx-squeezer/devkit:devkit ${project}`);
-    // const result = await runNxCommandAsync(`build ${project}`);
-    expect(project).not.toBeNull();
-  }, 120000);
+  it('should be installed', () => {
+    // npm ls will fail if the package is not installed properly
+    execSync('npm ls @nx-squeezer/devkit', { cwd: projectDirectory, stdio: 'inherit' });
+  });
 });
+
+/**
+ * Creates a test project with create-nx-workspace and installs the plugin
+ * @returns The directory where the test project was created
+ */
+function createTestProject() {
+  const projectDirectory = tmpProjPath();
+  const projectName = basename(projectDirectory);
+
+  // Ensure projectDirectory is empty
+  rmSync(projectDirectory, { recursive: true, force: true });
+  mkdirSync(dirname(projectDirectory), { recursive: true });
+
+  execSync(`npx --yes create-nx-workspace@latest ${projectName} --preset ts --nxCloud=skip --no-interactive`, {
+    cwd: dirname(projectDirectory),
+    stdio: 'inherit',
+    env: process.env,
+  });
+  console.log(`Created test project in "${projectDirectory}"`);
+
+  return projectDirectory;
+}
