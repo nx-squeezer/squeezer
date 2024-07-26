ec6617f4358eeb667851e0257514216be5b6cecb
Samuel Fernández <samuelfernandez@users.noreply.github.com>
2024-07-26 17:26:43 +0200

    chore: adjust migration script to fix issue when formatting

--- nx-migrate-latest.sh
+++ nx-migrate-latest.sh
@@ -11,7 +11,7 @@ npx --no-install nx migrate ng-packagr@latest
 npx --no-install nx migrate @angular-eslint/eslint-plugin@latest
 npx --no-install nx migrate @angular-eslint/eslint-plugin-template@latest
 npx --no-install nx migrate @angular-eslint/template-parser@latest
-npx --no-install nx migrate @ngrx/signals@next
+npx --no-install nx migrate @ngrx/signals@latest
 
 # Migrate Plugins
 npx --no-install nx migrate @jscutlery/semver@latest
@@ -25,6 +25,7 @@ npm install
 npx --no-install nx migrate --run-migrations --if-exists
 npm install
 rm -f migrations.json
+npx --no-install nx reset
 
 # Format
 npx --no-install nx run-many --target=lint --all --parallel=3 --fix --skip-nx-cache
